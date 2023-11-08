b46b20ff3e97756c954eba31057b253ab37e7e7b
oozcan <onurozcan95@gmail.com>
2023-11-08 11:48:05 -0800

    Dark mode yapılması

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,30 @@
-import {Stack,Box} from '@mui/material';
+import {Box, createTheme, Stack, ThemeProvider} from '@mui/material';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
 import Rightbar from './components/Rightbar';
 import Navbar from './components/Navbar';
 import Add from './components/Add';
+import { useState } from 'react';
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
   return (
-    <Box>
-      <Navbar></Navbar>
-      <Stack direction='row' spacing={2}>
-        <Sidebar></Sidebar>
-        <Feed></Feed>
-        <Rightbar></Rightbar>
-    </Stack>
-    <Add></Add>
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar setMode={setMode} mode={mode}/>
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
   );
 }
 
