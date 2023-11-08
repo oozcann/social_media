044345686d2fcc415ef84a4589e8e21eafb6ffc5
oozcan <onurozcan95@gmail.com>
2023-11-08 11:09:02 -0800

    Navbar'ın oluşturulması

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,18 @@
-
+import {Stack,Box} from '@mui/material';
+import Sidebar from './components/Sidebar';
+import Feed from './components/Feed';
+import Rightbar from './components/Rightbar';
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div>
-      Onur
-    </div>
+    <Box>
+      <Navbar></Navbar>
+      <Stack direction='row' spacing={2}>
+        <Sidebar></Sidebar>
+        <Feed></Feed>
+        <Rightbar></Rightbar>
+    </Stack>
+    </Box>
   );
 }
 
